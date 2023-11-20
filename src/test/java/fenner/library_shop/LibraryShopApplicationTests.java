8f79f4af6611ec489dbacdaaf500884f60473158
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2023-11-20 13:56:18 -0500

    :fire: First commit

---
+++ src/test/java/fenner/library_shop/LibraryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package fenner.library_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
